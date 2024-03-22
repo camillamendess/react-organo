bc0ae6579d945e8e9c58aea8913ca724eff35cbc
Camilla <milla_mendes83@hotmail.com>
2024-03-22 12:55:37 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+
+function Banner() {
+   // JSX
+ return (
+   <header className="banner">
+      <img src="./imagens/banner.png" alt="Banner principal da página do Organo"/>
+   </header>
+ );
+};
+
+export default Banner;
