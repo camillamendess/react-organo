718af07969ff34c784cd030c8c44843e875b7505
Camilla <milla_mendes83@hotmail.com>
2024-04-03 22:53:45 -0300

    interacting with the user

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
 import { useState } from "react";
 
-const Formulario = () => {
+const Formulario = (props) => {
   const times = [
     "Programaçao",
     "Front-End",
@@ -22,7 +22,12 @@ const Formulario = () => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("Form foi submetido => ", nome, cargo, imagem, time);
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
   };
 
   return (
