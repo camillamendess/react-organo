1d082afb7ac882819b29b4972876289ec15928d8
Camilla <milla_mendes83@hotmail.com>
2024-04-04 21:32:49 -0300

    creating time component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner/Banner";
 import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "Programaçao",
+      corPrimaria: "#57c278",
+      corSecundaria: "#d9f7e9",
+    },
+    {
+      nome: "Front-End",
+      corPrimaria: "#82cffa",
+      corSecundaria: "#e8f8ff",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "#a6d157",
+      corSecundaria: "#f0f8e2",
+    },
+    {
+      nome: "DevOps",
+      corPrimaria: "#e06b69",
+      corSecundaria: "#fde7e8",
+    },
+    {
+      nome: "UX e Design",
+      corPrimaria: "#db6ebf",
+      corSecundaria: "#fae9f5",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "#ffba05",
+      corSecundaria: "#fff5d9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#ff8a29",
+      corSecundaria: "#ffeedf",
+    },
+  ];
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -18,6 +57,15 @@ function App() {
           aoNovoColaboradorAdicionado(colaborador)
         }
       />
+
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }
